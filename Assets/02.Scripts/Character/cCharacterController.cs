731d10f767e5bed0dc371d380bdb3af95bcb57f3
LimCHBunkerDefense <gnwmapf@naver.com>
2017-07-25 14:39:48 +0900

    tes 변경

--- Assets/02.Scripts/Character/cCharacterController.cs
+++ Assets/02.Scripts/Character/cCharacterController.cs
@@ -52,6 +52,8 @@ public class cCharacterController : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+		print("테스트 창현");
+		print("asd");
         if (Input.GetMouseButton(0))
         {
             m_anim.SetBool("MoveToIdle", true);
